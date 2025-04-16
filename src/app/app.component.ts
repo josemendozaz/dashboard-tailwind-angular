2ce5bf674653eb72481554611818161275b3e97c
josemendozaz <josemendozaz1993@gmail.com>
2025-04-15 23:48:26 -0400

    Se inicia con la estructura de archivos, de igual forma se maqueta a grandes rasgos y de forma de guía, el template del dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { User } from '@models/user.model';
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector	: 'app-root',
+	// imports		: [RouterOutlet],
+	templateUrl	: './app.component.html',
+	styleUrl	: './app.component.css'
 })
 export class AppComponent {
-  title = 'dashboard-tailwind-angular';
+	title			= 'dashboard-tailwind-angular';
+	user	: User	= {
+		id: 0,
+		email: '',
+		name: '',
+		roles: []
+	}
 }
---
+++ src/app/shared/components/alert-modal/alert-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-modal',
+  imports: [],
+  templateUrl: './alert-modal.component.html',
+  styleUrl: './alert-modal.component.css'
+})
+export class AlertModalComponent {
+
+}
